ea74c7ba5fa5c1ea2d7e2acd7216bdc96cbbf628
cuongcau93 <cuongnm22@wru.vn>
2017-03-06 15:37:08 +0700

    Generic
    ver2

--- typescripts/training2/training2/Generics.js
+++ typescripts/training2/training2/Generics.js
@@ -22,5 +22,46 @@ var str5 = getCustomType({
     x: "Nguyen Manh Cuong",
     y: 10
 });
-console.log(typeof str5);
+console.log(str5.x);
+console.log("---Working with Generic Type Variables---");
+function variableGeneric(arg) {
+    return arg;
+}
+function variableGeneric2(arg) {
+    return arg[0];
+}
+function variableGeneric3(arg) {
+    return arg;
+}
+console.log("Example 1.1: " + variableGeneric("Nguyen manh cuong"));
+console.log("Example 1.2: " + variableGeneric2(["Nguyen", "Manh", "Cuong"]));
+console.log("Example 1.3: " + variableGeneric3(["Nguyen", "Manh", "Cuong"]));
+function identity(arg) {
+    return arg;
+}
+var myIdentity = identity;
+console.log("---Generic Classes---");
+var GenericNumber = (function () {
+    function GenericNumber() {
+    }
+    return GenericNumber;
+}());
+var myGenericNumber = new GenericNumber();
+var x1 = myGenericNumber.zeroValue = 10;
+var x2 = myGenericNumber.add = function (x, y) {
+    return x + y;
+};
+console.log("Example 2.1: " + x1);
+console.log("Example 2.2: " + x2(2, 3));
+var stringNumeric = new GenericNumber();
+var str11 = stringNumeric.zeroValue = "I love you";
+var str12 = stringNumeric.add = function (x, y) { return x + y; };
+console.log("Example 2.1: " + str11);
+console.log("Example 2.2: " + str12("hello ", "world"));
+function loggingIdentity(arg) {
+    console.log(arg.length);
+    return arg;
+}
+var str13 = loggingIdentity({ length: 10 });
+console.log("Example 2.3: " + str13);
 //# sourceMappingURL=Generics.js.map
--- typescripts/training2/training2/Generics.ts
+++ typescripts/training2/training2/Generics.ts
@@ -33,4 +33,54 @@ let str5 = getCustomType<CustomType>({
     y: 10
 })
 
-console.log(typeof str5);
+console.log(str5.x);
+console.log("---Working with Generic Type Variables---");
+function variableGeneric<T>(arg: T): T {
+    return arg;
+}
+function variableGeneric2<T>(arg: T[]): T {
+    return arg[0];
+}
+
+function variableGeneric3<T>(arg: Array<T>): Array<T> {
+    return arg;
+}
+ 
+console.log("Example 1.1: " +variableGeneric<string>("Nguyen manh cuong"));
+console.log("Example 1.2: " + variableGeneric2<string>(["Nguyen", "Manh", "Cuong"]));
+console.log("Example 1.3: " + variableGeneric3<string>(["Nguyen", "Manh", "Cuong"]));
+
+function identity<T>(arg: T): T {
+    return arg;
+}
+
+let myIdentity: <U>(arg: U) => U = identity;
+console.log("---Generic Classes---");
+class GenericNumber<T>{
+    zeroValue: T;
+    add: (x: T, y: T) => T;
+}
+
+let myGenericNumber = new GenericNumber<number>();
+let x1 = myGenericNumber.zeroValue = 10;
+let x2 = myGenericNumber.add = function (x, y) {
+    return x + y;
+};
+console.log("Example 2.1: " + x1);
+console.log("Example 2.2: " + x2(2, 3));
+let stringNumeric = new GenericNumber<string>();
+let str11 = stringNumeric.zeroValue = "I love you";
+let str12 = stringNumeric.add = (x, y) => x + y;
+console.log("Example 2.1: " + str11);
+console.log("Example 2.2: " + str12("hello ", "world"));
+
+interface Lengthwise {
+    length: number;
+}
+
+function loggingIdentity<T extends Lengthwise>(arg: T): T {
+    console.log(arg.length);
+    return arg;
+}
+let str13 = loggingIdentity({ length: 10 });
+console.log("Example 2.3: " + str13);
