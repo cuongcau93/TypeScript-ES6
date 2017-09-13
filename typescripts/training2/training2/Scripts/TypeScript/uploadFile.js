dd55552a373c4bdc702bbc1f05351235ef62cbce
cuongcau93 <cuongnm22@wru.vn>
2017-09-13 07:20:44 -0700

    upload

--- typescripts/training2/training2/Scripts/TypeScript/uploadFile.js
+++ typescripts/training2/training2/Scripts/TypeScript/uploadFile.js
@@ -1,6 +1,21 @@
 var Agenda = (function () {
     function Agenda() {
+        this.obj = $("#dragandrophandler");
+        this.wireEvents();
     }
+    Agenda.prototype.wireEvents = function () {
+        var _this = this;
+        $("input").change(function (event) {
+            console.log('aaa');
+            console.log(_this);
+            var files = $(".box__file").files;
+            console.log(files);
+            //handleFileUpload(files, obj);
+        });
+    };
     return Agenda;
 }());
+$(document).ready(function () {
+    var agenda = new Agenda();
+});
 //# sourceMappingURL=uploadFile.js.map
--- typescripts/training2/training2/Scripts/TypeScript/uploadFile.ts
+++ typescripts/training2/training2/Scripts/TypeScript/uploadFile.ts
@@ -1,9 +1,24 @@
 ﻿class Agenda {
 
-    constructor() {
+    obj: JQuery = $("#dragandrophandler");
+    files: any;
 
+    constructor() {
+        this.wireEvents();
     }
 
-
+    wireEvents() {
+        $("input").change(event => {
+            console.log('aaa');
+            console.log(this);
+            var files = $(".box__file").files;
+            console.log(files);
+            //handleFileUpload(files, obj);
+        });
+    }
 
 }
+
+$(document).ready(function () {
+    let agenda = new Agenda();
+});
