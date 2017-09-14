27ded2c25bdaba4af64c30873ceda2ebb2f43f85
cuongcau93 <cuongnm22@wru.vn>
2017-09-14 17:17:37 +0700

    update

--- typescripts/training2/training2/Scripts/TypeScript/uploadFile.js
+++ typescripts/training2/training2/Scripts/TypeScript/uploadFile.js
@@ -1,5 +1,6 @@
 var Agenda = (function () {
     function Agenda() {
+        //dataTransfer: DataTransfer;
         this.obj = $("#dragandrophandler");
         this.inputFile = $('input[type = "file"]');
         this.wireEvents();
@@ -7,14 +8,38 @@ var Agenda = (function () {
     Agenda.prototype.wireEvents = function () {
         var _this = this;
         this.inputFile.change(function (event) {
+            console.log(event);
             var files = _this.inputFile[0].files;
             console.log(files);
         });
-        $("#dragandrophandler").on('dragenter', function (event) {
-            event.stopPropagation();
-            event.preventDefault();
-            $(_this).css('border', '2px solid blue');
+        this.obj.on('dragenter', function (event) {
+            _this.notEventHandler(event);
+            $(_this.obj).css('border', '2px solid blue');
         });
+        this.obj.on('dragover', function (event) {
+            _this.notEventHandler(event);
+        });
+        this.obj.on('drop', function (event) {
+            $(_this).css('border', '2px dotted #0B85A1');
+            event.preventDefault();
+            var transfer = event.dataTransfer;
+            var files = event.originalEvent.dataTransfer.files;
+            console.log(files);
+        });
+        //this.obj.on('drop', (e) => {
+        //    let dataTransfer: DataTransfer;
+        //    $(this).css('border', '2px dotted #0B85A1');
+        //    e.preventDefault();
+        //    var files = e.originalEvent.dataTransfer.files;
+        //    //We need to send dropped files to Server
+        //    //handleFileUpload(files, obj);
+        //    //console.log(files);
+        //    //console.log(obj);
+        //});
+    };
+    Agenda.prototype.notEventHandler = function (event) {
+        event.stopPropagation();
+        event.preventDefault();
     };
     return Agenda;
 }());
--- typescripts/training2/training2/Scripts/TypeScript/uploadFile.ts
+++ typescripts/training2/training2/Scripts/TypeScript/uploadFile.ts
@@ -1,5 +1,5 @@
 ﻿class Agenda {
-
+    //dataTransfer: DataTransfer;
     obj: JQuery = $("#dragandrophandler");
     inputFile: any = $('input[type = "file"]');
 
@@ -10,17 +10,49 @@
     wireEvents() {
 
         this.inputFile.change(event => {
+            console.log(event);
             var files = this.inputFile[0].files
             console.log(files);
         });
 
-        $("#dragandrophandler").on('dragenter', event => {
-            event.stopPropagation();
-            event.preventDefault();
-            $(this).css('border', '2px solid blue');
+        this.obj.on('dragenter', event => {
+            this.notEventHandler(event);
+            $(this.obj).css('border', '2px solid blue');
         });
 
+        this.obj.on('dragover', event => {
+            this.notEventHandler(event);
+        });
+
+        this.obj.on('drop', (event: any) => {
+
+            $(this).css('border', '2px dotted #0B85A1');
+            event.preventDefault();
+            let transfer = event.dataTransfer;
+            var files = event.originalEvent.dataTransfer.files;
+            console.log(files);
+
+        });
+
+        //this.obj.on('drop', (e) => {
+        //    let dataTransfer: DataTransfer;
+        //    $(this).css('border', '2px dotted #0B85A1');
+        //    e.preventDefault();
+        
+        //    var files = e.originalEvent.dataTransfer.files;
+        //    //We need to send dropped files to Server
+        //    //handleFileUpload(files, obj);
+        //    //console.log(files);
+        //    //console.log(obj);
+
+        //});
     }
+
+    notEventHandler(event) {
+        event.stopPropagation();
+        event.preventDefault();
+    }
+
 }
 
 $(document).ready(function () {
