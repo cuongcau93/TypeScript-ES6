91022b847aa261a63701533012c6ac839c9a453c
cuongcau93 <cuongnm22@wru.vn>
2017-09-21 17:38:56 +0700

    update

--- typescripts/training2/training2/Scripts/TypeScript/sammyjs/app.js
+++ typescripts/training2/training2/Scripts/TypeScript/sammyjs/app.js
@@ -1,54 +1,22 @@
-//(function ($) {
-//    var app = $.sammy('#content', function () {
-//        /* load mustache plugin */
-//        this.use('Mustache', 'ms');
-//        var app = $.sammy('#content');
-//        app.get('#/filme', function (context) {
-//            this.load('filme.json')
-//                .then(function (items) {
-//                    $.each(items, function (i, item) {
-//                        context.render('template.ms', { id: item.id, name: item.name, genre: item.genre, year: item.year }).appendTo(context.$element());
-//                    });
-//                });
-//            //alert('aaaa');
-//        });
-//    });
-//    $(function () {
-//        app.run('#/');
-//    });
-//    //$('.next').on('click', function () {
-//    //    url = '#/filme';
-//    //    console.log(url);
-//    //    window.location = url;
-//    })
-//})(jQuery);
-var App = (function () {
-    function App() {
-        this.sammyApp().run('#/');
-    }
-    App.prototype.sammyApp = function () {
-        $.sammy('#content', function () {
-            /* load mustache plugin */
-            this.use('Mustache', 'ms');
-            var app = $.sammy('#content');
-            app.get('#/filme', function (context) {
-                alert('aaaa');
-                this.load('Api/filme.json')
-                    .then(function (items) {
-                    $.each(items, function (i, item) {
-                        context.render('View/template/template.ms', { id: item.id, name: item.name, genre: item.genre, year: item.year }).appendTo(context.$element());
-                    });
-                });
+var IRMeetingRequest;
+(function (IRMeetingRequest) {
+    'use strict';
+    var appMain = Sammy('#main', function () {
+        this.use('Mustache', 'ms');
+        var context = this;
+        this.get('#/', function () {
+            var context = this.context;
+            this.swap('Loaded content successfully!!!');
+        });
+        appMain.get('#/filme', function (context) {
+            context.render('template.ms', {}, function (output) {
+                $('#content').append(output);
             });
         });
-    };
-    return App;
-}());
-$(document).ready(function () {
-    var app = new App();
-    $('.next').on('click', function () {
-        var url = '#/filme';
-        console.log(url);
     });
-});
+    $(function () {
+        // start the app with home route
+        appMain.run('#/');
+    });
+})(IRMeetingRequest || (IRMeetingRequest = {}));
 //# sourceMappingURL=app.js.map
--- typescripts/training2/training2/Scripts/TypeScript/sammyjs/app.ts
+++ typescripts/training2/training2/Scripts/TypeScript/sammyjs/app.ts
@@ -1,67 +1,28 @@
-﻿//(function ($) {
-//    var app = $.sammy('#content', function () {
-//        /* load mustache plugin */
-//        this.use('Mustache', 'ms');
+﻿module IRMeetingRequest {
+    'use strict';
 
-//        var app = $.sammy('#content');
-
-//        app.get('#/filme', function (context) {
-
-//            this.load('filme.json')
-//                .then(function (items) {
-
-//                    $.each(items, function (i, item) {
-//                        context.render('template.ms', { id: item.id, name: item.name, genre: item.genre, year: item.year }).appendTo(context.$element());
-//                    });
-//                });
-//            //alert('aaaa');
-//        });
-//    });
-
-//    $(function () {
-//        app.run('#/');
-//    });
-
-//    //$('.next').on('click', function () {
-//    //    url = '#/filme';
-//    //    console.log(url);
-//    //    window.location = url;
-
-//    })
-
-
-//})(jQuery);
-class App {
-
-    constructor() {
-        this.sammyApp().run('#/');
-    }
-
-    sammyApp(): any {
-        $.sammy('#content', function () {
-        /* load mustache plugin */
+    let appMain = Sammy('#main', function () {
         this.use('Mustache', 'ms');
+        let context = this;
 
-        var app = $.sammy('#content');
+        this.get('#/', function () {
+            var context = this.context;
+            this.swap('Loaded content successfully!!!');
+        })
 
-        app.get('#/filme', function (context) {
-            alert('aaaa');
-            this.load('Api/filme.json')
-                .then(function (items) {
+        appMain.get('#/filme', function (context) {
 
-                    $.each(items, function (i, item) {
-                        context.render('View/template/template.ms', { id: item.id, name: item.name, genre: item.genre, year: item.year }).appendTo(context.$element());
-                    });
-                });
+            context.render('template.ms', {}, function (output) {
+                $('#content').append(output);
+            });
+
         });
-    });
-    }
-}
 
-$(document).ready(() => {
-    var app = new App();
-    $('.next').on('click', function () {
-        var url: string = '#/filme';
-        console.log(url);
+    });
+
+    $(function () {
+        // start the app with home route
+        appMain.run('#/')
     })
-})
+    
+}
